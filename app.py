a5d0b1e9fa222d50447e937552c8d4220f5691de
Manpreet Singh <manpreet@gmail.com>
2026-05-08 10:07:30 +0530

    Phase 5: GitHub Actions CI/CD pipeline added

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ def download():
     return send_file(path, as_attachment=True, download_name='cleaned_data.csv')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
